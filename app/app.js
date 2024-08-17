054d923dae854236f3a9ea3732904933b3762e40
Ignacio Oteiza <oteizanacho@gmail.com>
2024-08-17 15:16:01 -0300

    app.js actualizado

--- app/app.js
+++ app/app.js
@@ -2,51 +2,43 @@
 import Question from '../models/Question.js';
 import Player from '../models/Player.js';
 import Board from '../models/Board.js';
-import Box from '../models/Box.js';
 //importo json con las preguntas
 import questionsData from '../data/questions.json' assert { type: 'json' };
+import newQuestionData from '../data/newquestion.json' assert { type: 'json'};
 //universal id
 import { v4 as uuidv4 } from 'uuid';
 
-function getRandomColor(){
-    const r = Math.floor((Math.random() * 200) + 50)
-    const g = Math.floor((Math.random() * 200) + 50)
-    const b = Math.floor((Math.random() * 200) + 50)
-    //rbg(112,243,59)
-    return `rgb(${r},${g},${b})`
-}
-
 //creo jugador nuevo
-function newPlayer(id, name){
-    const player = new Player(id, name, getRandomColor());
+function newPlayer(id, name, color){
+    const player = new Player(id, name, color);
     return player;
 }
 
 //creo un tablero nuevo
 function newBoard(){
     //arreglo para guardar los casilleros
-    let boxes = [];
+    let boxes = {};
     for(let i = 0; i < 20; i++){
-        const questionid = uuidv4();
-        //creo una pregunta usando los datos traidos del archivo JSON de preguntas 
-        const question = new Question(questionid, questionsData[i].question, questionsData[i].options, questionsData[i].answer);
-        //creo un casillero nuevo
-        const box = new Box (i + 1, question);
-        //sumo el casillero al arreglo de casilleros
-        boxes.push(box);
+        boxes[i + 1] = new Question(questionsData[i].question, questionsData[i].options, questionsData[i].answer);
     }
     const idboard = uuidv4();
-    //creo un tablero nuevo y le paso los casilleros
     const board = new Board(idboard, boxes);
     return board;
+
 }
 
 function rollDice(){
     return Math.floor((Math.random() * 6));
 }
 
+function newQuestion(){
+    const newquestion = newQuestionData.pop();
+    return new Question(newquestion.question, newquestion.options, newquestion.answer);
+}
+
 export default {
     newPlayer,
     newBoard,
     rollDice,
+    newQuestion
 }
